06711e56eed878dff3d87f356385ae852a3157d5
Illia Lebid <illia.lebid@onsolve.com>
2022-05-16 02:49:14 +0300

    Extract POLLING_INTERVAL to env

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const BANK_BLACK_LIST = ['Wise'];
-const POLLING_INTERVAL = 60000;
+const POLLING_INTERVAL = process.env.POLLING_INTERVAL;
 const MAX_LIST_SIZE = 5;
 const ELEMENTS_PER_PAGE = 20;
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
