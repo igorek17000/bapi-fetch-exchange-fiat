06889700e9f1c95cf4671e49fc29897006165c37
Illia Lebid <illia.lebid@onsolve.com>
2022-05-16 02:43:39 +0300

    Rework polling

--- api/telegram.js
+++ api/telegram.js
@@ -1,41 +1,48 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { getP2PData } = require('../api/binance');
 const { normalizeElements } = require('../utils/normalizeElements');
-const { POLLING_INTERVAL, BANK_BLACK_LIST, MAX_LIST_SIZE, ELEMENTS_PER_PAGE, TELEGRAM_TOKEN } = require('../utils/constants');
+const { BANK_BLACK_LIST, MAX_LIST_SIZE, ELEMENTS_PER_PAGE, TELEGRAM_TOKEN, POLLING_INTERVAL } = require('../utils/constants');
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
 const initiate = () => {
-    const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: { interval: POLLING_INTERVAL } });
-
     bot.onText(/^\/start$/, async (msg) => {
-        const chatId = msg.chat.id;
+        const poll = (promiseFn, time) =>
+            promiseFn().then(async (firstPage) => {
+                if (firstPage && firstPage.success) {
+                    const chatId = msg.chat.id;
+                    const totalPages = Math.ceil((firstPage.total / ELEMENTS_PER_PAGE));
+                    const pages = new Array(totalPages - 1).fill(null);
+                    const elements = await pages.reduce(async (prev, _, idx) => {
+                        const accData = await prev;
+                        const page = idx + 2;
 
-        setInterval(async () => {
-            const firstPage = await getP2PData();
+                        const result = await getP2PData(page)
 
-            if (firstPage && firstPage.success) {
-                const totalPages = Math.ceil((firstPage.total / ELEMENTS_PER_PAGE));
-                const pages = new Array(totalPages - 1).fill(null);
-                const elements = await pages.reduce(async (prev, _, idx) => {
-                    const accData = await prev;
-                    const page = idx + 2;
+                        if (result && result.success) {
+                            return [...accData, ...result.data]
+                        }
 
-                    const result = await getP2PData(page)
+                        return accData;
+                    }, Promise.resolve(firstPage.data))
 
-                    if (result && result.success) {
-                        return [...accData, ...result.data]
+                    if (elements) {
+                        const elementsConfigSize = { start: 0, end: MAX_LIST_SIZE };
+                        const normalizedElements = normalizeElements(elements, BANK_BLACK_LIST, elementsConfigSize)
+
+                        bot.sendMessage(chatId, normalizedElements)
                     }
-
-                    return accData;
-                }, Promise.resolve(firstPage.data))
-
-                if (elements) {
-                    const elementsConfigSize = { start: 0, end: MAX_LIST_SIZE };
-                    const normalizedElements = normalizeElements(elements, BANK_BLACK_LIST, elementsConfigSize)
-
-                    bot.sendMessage(chatId, normalizedElements)
                 }
-            }
-        }, POLLING_INTERVAL);
+
+                return sleep(time).then(() => {
+                    return poll(promiseFn, time)
+                })
+            })
+
+        poll(getP2PData, POLLING_INTERVAL);
+
     });
 }
 
